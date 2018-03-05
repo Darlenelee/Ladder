50f29e6fe4347dbfb731e8ac94188022af77b242
Darlenelee <1215664347@qq.com>
2018-03-05 20:45:02 +0800

    read files

--- Ladder.java
+++ Ladder.java
@@ -1,15 +1,41 @@
 import java.io.*;
+import java.util.Iterator;
+import java.util.HashSet;
+
+
 public class Ladder
 {
 	public static void main(String[] args)
 	{
-		String s = "";
-		System.out.print("Dictionary file name? ");
-		try{
-			BufferedReader in = new BufferedReader(
-				new InputStreamReader(System.in));
-			s = in.readLine();
-		}catch(IOException e){}
-		System.out.println("You have entered: "+s);
+		HashSet dic = new HashSet();
+
+		String fname = "";
+
+		FileReader out = null;
+		while(true){
+            try{
+                System.out.print("Dictionary file name? ");
+                BufferedReader in = new BufferedReader(
+					new InputStreamReader(System.in));
+			    fname = in.readLine();
+
+                // create a file object with filename
+                out = new FileReader(fname);
+                break;
+            }catch(FileNotFoundException ex){
+                System.out.println("Unable to open that file.   Try again.\n");
+            }
+            catch(IOException e){
+                System.out.println("Unable to open that file.   Try again.\n");
+            }
+		}
+
+
+//		try{
+//			BufferedReader in = new BufferedReader(
+//				new InputStreamReader(System.in));
+//			fname = in.readLine();
+//		}catch(IOException e){}
+//		System.out.println("You have entered: "+s);
 	}
 }
